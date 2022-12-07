697a280fb72787800adc778d3b064c9f4856104e
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-12-07 07:49:34 +0100

    0.0.2

--- ToolToAutomatizationProcesses/AutomationTool/AutomationTool/Form1.cs
+++ ToolToAutomatizationProcesses/AutomationTool/AutomationTool/Form1.cs
@@ -26,7 +26,11 @@ namespace AutomationTool
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Zmienia wyświetlany form w oknie głównym 
+        /// </summary>
+        /// <param name="ChangeTo"></param>
+        /// <param name="ChangeIn"></param>
         void ChangeDisplayForm(Form ChangeTo, Panel ChangeIn)
         {
             ChangeTo.TopLevel = false;
@@ -38,37 +42,70 @@ namespace AutomationTool
             ChangeTo.Show();
         }
        
+        /// <summary>
+        /// Przełącza boczny form na ustawienia całego procesu 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_StartProcessing_Click(object sender, EventArgs e)
         {
             //DownloadImages();
             ChangeDisplayForm(new AutomationTool.AllSettings(this), this.panel2);
         }
 
+        /// <summary>
+        /// Przełącza form boczny na ustawienia tylko pobierania 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_OnlyInstall_Click(object sender, EventArgs e)
         {
             //DownloadImages();
             ChangeDisplayForm(new AutomationTool.OnlyInstallSettings(this), this.panel2);
         }
 
+        /// <summary>
+        /// Przełącza na ustawienia formu, który sprawdza czy zdjęcia są zdublowane
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_ChceckIsDubled_Click(object sender, EventArgs e)
         {
             ChangeDisplayForm(new AutomationTool.Settings.IsDublet(this), this.panel2);
         }
 
+        /// <summary>
+        /// Przełącza na ustawienia formu, który obcina zdjęcia
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_CutOnly_Click(object sender, EventArgs e)
         {
             ChangeDisplayForm(new AutomationTool.Settings.CutOnly(this), this.panel2);
         }
-
+        
+        /// <summary>
+        /// Przełącza na ustawienia formu, który zmienia nazwy plików
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_ChangeNames_Click(object sender, EventArgs e)
         {
             ChangeDisplayForm(new AutomationTool.Settings.ChangeName(this), this.panel2);
         }
 
+        /// <summary>
+        /// Funkcja zaczyna proces, który zostaje podany w Which
+        /// </summary>
+        /// <param name="Which"></param>
+        /// <param name="RunInfo"></param>
         public void RunFunction(WhichFun Which, FormDataConstructor RunInfo)
         {
+            //Wszystkie rozpoczęcia są tworzone na nowym wątku, aby główne okno było responsywne 
+            //oraz, aby zespawnowane okno również było responsywne 
             switch(Which)
             {
+                //Zaczyna proces sprawdzania zdublowania zdjęć
                 case WhichFun.DoubleSearcer :
                     {
                         TaskForm nowy = new TaskForm();
@@ -85,6 +122,7 @@ namespace AutomationTool
                         break;
                     }
 
+                //Zaczyna proces pobierania zdjęć
                 case WhichFun.Download:
                     {
                         TaskForm nowy = new TaskForm();
@@ -101,6 +139,7 @@ namespace AutomationTool
                         break;
                     }
 
+                //Zaczyna proces zmieniania nazw zdjęć
                 case WhichFun.Rename:
                     {
                         TaskForm nowy = new TaskForm();
@@ -117,6 +156,7 @@ namespace AutomationTool
                         break;
                     }
 
+                //Zaczyna proces skalowania zdjęć
                 case WhichFun.Scaler:
                     {
                         TaskForm nowy = new TaskForm();
@@ -136,6 +176,12 @@ namespace AutomationTool
             }
         }
 
+        /// <summary>
+        /// Przeciążenie funkcji RunFunction, aby móc pobierać tylko zdjęcia
+        /// </summary>
+        /// <param name="Which"></param>
+        /// <param name="RunInfo"></param>
+        /// <param name="ListaSlow"></param>
         public void RunFunction(WhichFun Which, FormDataConstructor RunInfo, List<string> ListaSlow)
         {
             switch (Which)
@@ -158,6 +204,12 @@ namespace AutomationTool
             }
         }
 
+        /// <summary>
+        /// Przeciążenie funkcji RunFunction, aby móc scalować tylko zdjęcia
+        /// </summary>
+        /// <param name="Which"></param>
+        /// <param name="RunInfo"></param>
+        /// <param name="ListaSlow"></param>
         public void RunFunction(WhichFun Which, FormDataConstructor RunInfo, Size toSize)
         {
             switch(Which)
